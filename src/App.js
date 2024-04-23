80a623985bec52e3732c55d49d7617c4a27b2ef7
nrz123 <2311286220@qq.com>
2024-04-23 14:08:57 +0800

    一些

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import Home from './home.js'
+import MainPage from './mainpage.js'
+import Employment from './employment.js'
+import MakeUp from './makeup.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+            <Route exact path="/" element={<MainPage />}/>
+            <Route path="/employment" element={<Employment />}/>
+            <Route path="/employment/makeup" element={<MakeUp />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
+export default App
